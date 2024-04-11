a5fc94cc6263fec4eb392b766f5a3ef83f9943f5
santino <ing.roquezegarra@gmail.com>
2024-04-11 11:02:37 -0500

    homepage v1.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://roquez12.github.io/homepage-news",
+  base:"https://roquez12.github.io/HomePage-News",
 })
